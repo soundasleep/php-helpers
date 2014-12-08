b4ca857d82eac9c70e6016d2d6f3371a797d9433
Jevon Wright <jevon@jevon.org>
2014-12-08 15:27:48 +1300

    Adding is_valid_url() and is_valid_email() helpers

--- functions.php
+++ functions.php
@@ -41,3 +41,23 @@ function redirect($url) {
   header('Location: ' . $url);
   die();
 }
+
+/**
+ * Uses PHP's filter_var() to validate e-mail addresses, and also ensures the e-mail address
+ * is shorter than 255 characters (limit in our database for e-mail addresses).
+ *
+ * NOTE this requires MySQL 5+ which uses VARCHAR(32) to define 32 characters, not 32 bytes,
+ * therefore we can use mb_strlen().
+ *
+ * To support UTF-8 email addresses, we aren't too picky about edge cases; if it looks like
+ * an e-mail address, accept it.
+*/
+function is_valid_email($e) {
+  return mb_strlen($e) <= 255 && preg_match("/^[^@ ]+@([^@ ]+\\.)+[^@ ]+$/u", $e);
+}
+
+function is_valid_url($e) {
+  $e = mb_strtolower($e);
+  return mb_strlen($e) <= 255 &&
+    (mb_substr($e, 0, mb_strlen("http://")) == "http://" || mb_substr($e, 0, mb_strlen("https://")) == "https://");
+}
